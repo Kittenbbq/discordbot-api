febac1a163f056960dc5c1649801909cc289f748
Jussi Piirainen <j.piirainen@hotmail.fi>
2017-05-21 21:59:29 +0300

    allow * (cors)

--- src/main/java/api/Config/SecurityConfig.java
+++ src/main/java/api/Config/SecurityConfig.java
@@ -3,21 +3,22 @@ package api.Config;
 
 import kittenbbq.discordbot.BotConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private BotConfig config;
 
     public SecurityConfig() {
-        config = new BotConfig();
+
     }
 
-    // @formatter:off
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -29,6 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        BotConfig config = new BotConfig();
+        System.out.println("USERNAME: " + config.getApiUsername());
+        System.out.println("PW: " + config.getApiPassword());
         auth
                 .inMemoryAuthentication()
                 .withUser(config.getApiUsername()).password(config.getApiPassword()).roles("USER");
--- src/main/java/api/Controllers/MessageController.java
+++ src/main/java/api/Controllers/MessageController.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class MessageController {
     public MessageController() {
         this.bot = new BotBase();
