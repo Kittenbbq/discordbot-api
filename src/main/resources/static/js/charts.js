4670dd1e3865e57281e5d2d2e9f14a5d2b4c7110
Jussi Piirainen <j.piirainen@hotmail.fi>
2017-05-21 22:00:07 +0300

    chart styling

--- src/main/resources/static/js/charts.js
+++ src/main/resources/static/js/charts.js
@@ -1,101 +1,129 @@
+let srv = "http://192.168.1.102:8080";
+let globals = {
+	bgColor: "#333c43",
+	mainColor: "#e16224",
+	lightColor: "#f0b091",
+	fontStyle: {
+		fontFamily: "Verdana, Geneva, sans-serif",
+		color: "#CACACA"
+	},
+	titleFontStyle: {
+
+	}
+};
+
 $( document ).ready( () => {
+	Highcharts.setOptions( {
+		chart: {
+				fontFamily: "\"Verdana\", Geneva, sans-serif",
+				color: "#FFFFFF"
+		}
+	} );
 	loadCharts();
-});
+} );
 
 function loadCharts() {
-	console.log("Loading charts");
+	console.log( "Loading charts" );
 
 	// Message info
-	$.get("/api/messages/info", (data) => {
-		console.log(data);
-		$( "#totalMessageCount" ).append(data.messageCount);
-		$( "#firstMessage" ).append(data.firstMessage);
-		$( "#lastMessage" ).append(data.lastMessage);
-	});
+	$.get( `${srv}/api/messages/info`, ( data ) => {
+		console.log( data );
+		$( "#totalMessageCount" ).append( data.messageCount );
+		$( "#firstMessage" ).append( data.firstMessage );
+		$( "#lastMessage" ).append( data.lastMessage );
+	} );
 
 	// Top 10 posters
-	$.get("/api/messages/countByAuthor", (data) => {
-		console.log(data);
-		for (let i = 0; i < data.length; i++) {
-			if (i + 1 > 10) {
+	$.get( `${srv}/api/messages/countByAuthor`, ( data ) => {
+		console.log( "data" );
+		for ( let i = 0; i < data.length; i++ ) {
+			if ( i + 1 > 10 ) {
 				break;
 			}
-			$( "#topMessages" ).append(
-				"<p>" + (i + 1) + " " + data[i].authorName + ": " + data[i].messageCount + "</p>"
-			);
+			$( "#topMessages" ).append( `
+				<p>${( i + 1 )} ${data[ i ].authorName}: ${data[ i ].messageCount}</p>
+			` );
 		}
-	});
+	} );
 
 	// Top 10 posters
-	$.get("/api/messages/countByDate", (data) => {
+	$.get( `${srv}/api/messages/countByDate`, ( data ) => {
 		let chartData = [];
-		for (let entry of data) {
-			chartData.push([
-					Date.parse(entry.date),
-					entry.messageCount
-			]);
+		for ( let entry of data ) {
+			chartData.push( [
+				Date.parse( entry.date ),
+				entry.messageCount
+			] );
 		}
 
-		Highcharts.chart("messageCountByDate", {
-		    title: {
-		        text: "Message count by date"
-		    },
-		    subtitle: {
-		        text: "ObeseFinns Discord"
-		    },
-		    yAxis: {
-		        title: {
-		            text: "Messages sent"
-		        }
-		    },
+		Highcharts.chart( "messageCountByDate", {
+			chart: {
+				backgroundColor: globals.bgColor,
+				style: {
+					color: "\"#fff\"",
+					fontFamily: "\"Verdana\", \"Geneva\", \"sans-serif\"",
+				}
+			},
+			title: {
+				text: "Message count by date"
+			},
+			subtitle: {
+				text: "ObeseFinns Discord"
+			},
+			yAxis: {
+				title: {
+					text: "Messages sent"
+				},
+				min: 0
+			},
 			xAxis: {
 				type: "datetime",
 				title: {
 					text: "Date"
 				}
 			},
-		    legend: {
-		        layout: "vertical",
-		        align: "right",
-		        verticalAlign: "middle"
-		    },
+			legend: {
+				layout: "vertical",
+				align: "right",
+				verticalAlign: "middle"
+			},
 			plotOptions: {
-	            area: {
-	                fillColor: {
-	                    linearGradient: {
-	                        x1: 0,
-	                        y1: 0,
-	                        x2: 0,
-	                        y2: 1
-	                    },
-	                    stops: [
-	                        [0, Highcharts.getOptions().colors[0]],
-	                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-	                    ]
-	                },
-	                marker: {
-	                    radius: 2
-	                },
-	                lineWidth: 1,
-	                states: {
-	                    hover: {
-	                        lineWidth: 1
-	                    }
-	                },
-	                threshold: null
-	            }
-	        },
-		    series: [{
+				area: {
+					fillColor: {
+						linearGradient: {
+							x1: 0,
+							y1: 0,
+							x2: 0,
+							y2: 1
+						},
+						stops: [
+							[ 0, globals.mainColor ],
+							[ 1, "rgba( 225, 98, 36, 0.1 )" ]
+						]
+					},
+					marker: {
+						radius: 2
+					},
+					lineWidth: 1,
+					states: {
+						hover: {
+							lineWidth: 1
+						}
+					},
+					threshold: null
+				}
+			},
+			series: [ {
 				type: "area",
 				name: "Count",
+				color: globals.mainColor,
 				data: chartData
-			}]
-		});
+			} ]
+		} );
+	} );
 
-	});
 
-
-	$.get("/api/messages/countByDayHour", (data) => {
+	$.get( `${srv}/api/messages/countByDayHour`, ( data ) => {
 		let chartData = [];
 		let weekdays = [
 			"Monday",
@@ -104,56 +132,57 @@ function loadCharts() {
 			"Thursday",
 			"Friday",
 			"Saturday",
-			"Sunday",
+			"Sunday"
 		];
 
 		let categories = [];
-		for (let day of weekdays) {
-			for (let i = 0; i < 24; i++) {
-				let hourString = i.length == 1 ? "0" + i.ToString() : i;
-				console.log("day is " + day);
-				categories.push(day + " " + hourString + ":00");
-				let value = data.find( (entry) => {
-					return entry.day == day && entry.hour == i;
-				});
-				chartData.push(value ? value.messageCount : 0);
+		for ( let day of weekdays ) {
+			for ( let i = 0; i < 24; i++ ) {
+				let hourString = i.length === 1 ? `0${i}` : i;
+				console.log( "day is " + day );
+				categories.push( `${day} ${hourString}:00` );
+				let value = data.find( ( entry ) => {
+					return entry.day === day && entry.hour === i;
+				} );
+				chartData.push( value ? value.messageCount : 0 );
 			}
 		}
 
-		console.log(categories);
-		console.log(chartData);
+		console.log( categories );
+		console.log( chartData );
 
-		Highcharts.chart("messageCountByDayHour", {
+		Highcharts.chart( "messageCountByDayHour", {
 			chart: {
-				type: "area"
+				backgroundColor: globals.bgColor,
+				type: "spline"
 			},
-		    title: {
-		        text: "Message count by weekday"
-		    },
-		    subtitle: {
-		        text: "ObeseFinns Discord"
-		    },
-		    yAxis: {
-		        title: {
-		            text: "Messages sent"
-		        }
-		    },
+			title: {
+				text: "Message count by weekday"
+			},
+			subtitle: {
+				text: "ObeseFinns Discord"
+			},
+			yAxis: {
+				title: {
+					text: "Messages sent"
+				}
+			},
 			xAxis: {
 				categories: categories,
 				title: {
 					text: "Hour"
 				}
 			},
-		    legend: {
-		        layout: "vertical",
-		        align: "right",
-		        verticalAlign: "middle"
-		    },
-		    series: [{
+			legend: {
+				layout: "vertical",
+				align: "right",
+				verticalAlign: "middle"
+			},
+			series: [ {
 				name: "Count",
+				color: globals.mainColor,
 				data: chartData
-			}]
-		});
-
-	});
+			} ]
+		} );
+	} );
 }
