dff3ba2b4802430fbab7dba054b8562d0034f667
Jussi Piirainen <j.piirainen@hotmail.fi>
2017-05-29 20:39:09 +0300

    implemented url parsing for GET /messages/withUrl

--- src/main/java/api/Controllers/MessageController.java
+++ src/main/java/api/Controllers/MessageController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -215,13 +216,15 @@ public class MessageController {
      * Gets messages
      */
     @RequestMapping("/api/messages/withUrl")
-    public List<String> getMessagesWithUrl(
+    public List<UrlHitCount> getMessagesWithUrl(
             @RequestParam(value = "fromDate", defaultValue = "2010-01-01") String fromDate,
             @RequestParam(value = "toDate", defaultValue = "2030-01-01") String toDate
     ) {
         List<String> messages = new ArrayList<>();
         ResultSet results;
         PreparedStatement statement;
+        Map<String, Integer> urls = new HashMap<>();
+        List<UrlHitCount> sortedUrls = new ArrayList<>();
 
         try {
             statement = db.getConn().prepareStatement(
@@ -232,11 +235,40 @@ public class MessageController {
                 messages.add(results.getString("content"));
             }
 
-            // TODO: PARSE RESULTS
+
+            Pattern p = Pattern.compile("(?<=^| )https?:\\/\\/\\S*?(?=\\/)");
+            System.out.println("url results");
+            for (String item : messages) {
+                // Get url matches in message content
+                Matcher m = p.matcher(item);
+                while(m.find()) {
+                    // Get relevant part
+                    String url = m.group().replaceAll("https?:\\/\\/","");
+                    url = url.replaceAll("www\\.","");
+                    url = url.substring(0, url.length());
+
+                    // Check if already exists
+                    if (urls.containsKey(url)) {
+                        urls.put(url, urls.get(url) + 1);
+                    } else {
+                        urls.put(url, 1);
+                    }
+                }
+            }
+
+            // Sort results
+            for (Map.Entry<String, Integer> val : urls.entrySet()) {
+                UrlHitCount tmpUrl = new UrlHitCount();
+                tmpUrl.setUrl(val.getKey());
+                tmpUrl.setHits(val.getValue());
+                sortedUrls.add(tmpUrl);
+            }
+
+            Collections.sort(sortedUrls, (first, second) -> first.getHits() > second.getHits() ? -1 : (first.getHits() < second.getHits()) ? 1 : 0);
         } catch(Exception e) {
             System.out.println("Database query failed: : " + e.getMessage());
         }
-        return messages;
+        return sortedUrls;
     }
 
 
---
+++ src/main/java/api/Models/UrlHitCount.java
@@ -0,0 +1,22 @@
+package api.Models;
+
+public class UrlHitCount {
+    private String url;
+    private int hits;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public void setHits(int hits) {
+        this.hits = hits;
+    }
+}
